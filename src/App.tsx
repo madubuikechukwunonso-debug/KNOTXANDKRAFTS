91bd1fc55cbb866419d5eb497c52deecab2415a0
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-22 00:59:42 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Routes, Route } from 'react-router'
-import { CartProvider } from './hooks/useCart'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Shop from './pages/Shop'
-import Booking from './pages/Booking'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Admin from './pages/Admin'
-import Account from './pages/Account'
-import NotFound from './pages/NotFound'
+import { Routes, Route } from "react-router";
+import { CartProvider } from "./hooks/useCart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Shop from "./pages/Shop";
+import Booking from "./pages/Booking";
+import Gallery from "./pages/Gallery";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Admin from "./pages/Admin";
+import Account from "./pages/Account";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <CartProvider>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/account" element={<Account />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </CartProvider>
-  )
+  );
 }
