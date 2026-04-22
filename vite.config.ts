ae7dff562d7fd65fcb016f29fb98bf2f159a5957
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-21 22:23:36 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@contracts': fileURLToPath(new URL('./contracts', import.meta.url)),
-      '@db': fileURLToPath(new URL('./db', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@contracts": fileURLToPath(new URL("./contracts", import.meta.url)),
+      "@db": fileURLToPath(new URL("./db", import.meta.url)),
     },
   },
   build: {
-    outDir: './dist/public',   // ← this is the only change
+    outDir: "dist",
   },
 });
