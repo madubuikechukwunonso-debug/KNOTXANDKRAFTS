0a8df5a72cfa2ee7ec7fca5d141afc604b0e84c2
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-22 23:00:47 -0700

    Update connection.ts

--- server/queries/connection.ts
+++ server/queries/connection.ts
@@ -1,18 +1,26 @@
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 import { env } from "../lib/env";
 import * as schema from "../../db/schema";
 import * as relations from "../../db/relations";
 
 const fullSchema = { ...schema, ...relations };
 
-let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
+let instance: ReturnType<typeof drizzle<typeof fullSchema>> | undefined;
+let pool: mysql.Pool | undefined;
 
 export function getDb() {
   if (!instance) {
-    instance = drizzle(env.databaseUrl, {
-      mode: "planetscale",
+    pool = mysql.createPool({
+      uri: env.databaseUrl,
+      connectionLimit: 10,
+      enableKeepAlive: true,
+    });
+
+    instance = drizzle(pool, {
       schema: fullSchema,
     });
   }
+
   return instance;
 }
