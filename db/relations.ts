8f1fff7f05043f0d0b69b501bf16ea62a777e00d
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 03:46:50 -0700

    Update relations.ts

--- db/relations.ts
+++ db/relations.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { orders, orderItems, products } from "./schema.js";
+import { orders, orderItems, products } from "./schema";
 
 export const ordersRelations = relations(orders, ({ many }) => ({
   items: many(orderItems),
