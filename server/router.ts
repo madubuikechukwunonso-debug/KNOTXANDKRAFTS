13cb13470654940b40d10645c8e950bc16ddf864
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 02:07:19 -0700

    Update router.ts

--- server/router.ts
+++ server/router.ts
@@ -1,12 +1,14 @@
-import { productRouter } from "./modules/product/router";
-import { orderRouter } from "./modules/order/router";
-import { bookingRouter } from "./modules/booking/router";
-import { heroImageRouter } from "./modules/hero-image/router";
-import { subscriberRouter } from "./modules/subscriber/router";
-import { userRouter } from "./modules/user/router";
-import { contactRouter } from "./modules/contact/router";
-import { paymentRouter } from "./modules/payment/router";
-import { createRouter, publicQuery } from "./middleware";
+import { authRouter } from "./modules/auth/router.js";
+import { localAuthRouter } from "./modules/auth/local-router.js";
+import { productRouter } from "./modules/product/router.js";
+import { orderRouter } from "./modules/order/router.js";
+import { bookingRouter } from "./modules/booking/router.js";
+import { heroImageRouter } from "./modules/hero-image/router.js";
+import { subscriberRouter } from "./modules/subscriber/router.js";
+import { userRouter } from "./modules/user/router.js";
+import { contactRouter } from "./modules/contact/router.js";
+import { paymentRouter } from "./modules/payment/router.js";
+import { createRouter, publicQuery } from "./middleware.js";
 
 export const appRouter = createRouter({
   ping: publicQuery.query(() => ({
@@ -15,6 +17,8 @@ export const appRouter = createRouter({
     env: process.env.NODE_ENV,
   })),
 
+  auth: authRouter,
+  localAuth: localAuthRouter,
   product: productRouter,
   order: orderRouter,
   booking: bookingRouter,
